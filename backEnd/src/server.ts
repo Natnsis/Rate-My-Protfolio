38a431a7049be9d42e100abba9d1567e32726edd
natnael <nsisay49@gmail.com>
2025-10-15 07:48:49 +0300

    models

--- backEnd/src/server.ts
+++ backEnd/src/server.ts
@@ -1,12 +1,10 @@
 import express, { Request, Response } from 'express';
-import { PrismaClient } from '@prisma/client';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
-const prisma = new PrismaClient();
 
 app.use(cors());
 app.use(express.json());
@@ -16,17 +14,6 @@ app.get('/', (_req: Request, res: Response) => {
   res
     .status(200)
     .json({ message: '✅ Server running with TypeScript + Prisma!' });
-});
-
-// GET /users
-app.get('/users', async (_req: Request, res: Response) => {
-  try {
-    const users = await prisma.user.findMany({ include: { posts: true } });
-    res.json(users);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Failed to fetch users' });
-  }
 });
 
 const PORT = process.env.PORT || 3000;
