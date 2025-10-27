b0f1011857009c5943ebdfb828768d3ff89b3059
Natnael Sisay <nsisay49@gmail.com>
2025-10-27 11:29:59 +0000

    cors origins for vercel fronend

--- backEnd/src/server.ts
+++ backEnd/src/server.ts
@@ -12,19 +12,38 @@ dotenv.config();
 
 const app = express();
 
-//middlewares
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+// Allowed origins
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://rate-my-protfolio-1.vercel.app',
+];
+
+// CORS middleware
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(cookieParser());
 
-//routes
+// Routes
 app.use('/api/v1', AuthRoute);
 app.use('/api/v1', PostRoute);
 app.use('/api/v1', LikeRoute);
 app.use('/api/v1', CommentRoute);
 app.use('/api/v1', AiRoute);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server ready at http://localhost:${PORT}`);
