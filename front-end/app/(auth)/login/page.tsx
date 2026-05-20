4e9834b2b2c20a592f40b142da21d9e177209ab3
Natnael <nsisay49@gmail.com>
2026-05-20 20:56:53 +0300

    login page

--- front-end/app/(auth)/login/page.tsx
+++ front-end/app/(auth)/login/page.tsx
@@ -1,7 +1,41 @@
 import React from "react";
+import Image from "next/image";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className="h-screen w-screen flex items-center justify-center">
+      <div className="p-5 border flex gap-10 items-center">
+        <div>
+          <Image
+            src="/rmp2.png"
+            alt="login-image"
+            width={200}
+            height={200}
+            className="h-[60vh] w-[30vw]"
+          />
+        </div>
+
+        <div className="w-[45vw] flex flex-col">
+          <h1 className="text-4xl">Login Page</h1>
+          <p className="text-muted-foreground">
+            Add your credentials to get started
+          </p>
+
+          <form>
+            <div className="flex flex-col">
+              <label>Email</label>
+              <input className="border-2" />
+            </div>
+
+            <div className="flex flex-col">
+              <label>Password</label>
+              <input className="border-2" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default page;
