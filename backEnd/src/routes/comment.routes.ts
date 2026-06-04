045bc2c43687a069470bc9431f3a2946ffc6d893
Natnael <nsisay49@gmail.com>
2026-06-04 23:15:36 +0300

    my theme

--- backEnd/src/routes/comment.routes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { fetchComment, sendComment } from '../controllers/comment.controller';
-const router = express.Router();
-
-router.get('/comments/:id', fetchComment);
-router.post('/comments/:id', sendComment);
-
-export default router;
--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -1,23 +1,6 @@
-import Brands from "@/components/Brands";
-import Features from "@/components/Freatures";
-import Header from "@/components/Header";
-import Hero from "@/components/Hero";
-import LiveTrending from "@/components/LiveTrending";
-import PortfolioBattles from "@/components/PortfolioBattles";
-import SocialProof from "@/components/SocialProof";
-
-const page = () => {
-  return (
-    <div className="px-5 py-b h-screen overflow-y-auto">
-      <Header />
-      <Hero />
-      <Brands />
-      <Features />
-      <SocialProof />
-      <LiveTrending />
-      <PortfolioBattles />
-    </div>
-  );
+const App = () => {
+  return <div>header</div>;
 };
 
-export default page;
+export default App;
+
